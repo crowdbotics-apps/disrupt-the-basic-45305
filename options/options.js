d4b883f748bd076c378104bc2d4a8c653a4b7da7
Crowdbotics <team@crowdbotics.com>
2024-01-07 03:07:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "disrupt-the-basic-45305",
+  url: "https://disrupt-the-basic-45305.botics.co",
+  api: "https://disrupt-the-basic-45305.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
